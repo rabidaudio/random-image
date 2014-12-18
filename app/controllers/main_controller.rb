4a9933eefb75401858e7840352fe8a8e854aeb2e
Charles Julian Knight <charles@rabidaudio.com>
2014-12-17 23:08:32 -0500

    Pulled other useful info

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,20 @@
 class MainController < ApplicationController
   def rand
-    doc = Nokogiri::HTML(open( FLICKR_ADDR ))
-    photo = (doc.css( FLICKR_PHOTO_CLASS )).first
-    p_src = photo.css("img").first.attr("src").gsub(/_m\./, "_b.")
+    p_src = _get_img()[:src]
     #redirect_to p_src
     data = Net::HTTP.get URI(p_src)
 
     params[:opacity] = 0 if params[:opacity].nil?
     blob = ImageProcess::darken(data, params[:opacity].to_f / 100)
     send_data blob, :type => 'image/jpg', :disposition => 'inline'
+  end
+  
+  def _get_img
+    doc = Nokogiri::HTML(open( FLICKR_ADDR ))
+    photo = (doc.css( FLICKR_PHOTO_CLASS )).first
+    p_src = photo.css("img").first.attr("src").gsub(/_m\./, "_b.")
+    p_link = FLICKR_DOMAIN + photo.css("a").first.attr("href")
+    p_title = photo.css("a").first.attr("title")
+    { src: p_src, link: p_link, title: p_title}
   end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
 FLICKR_ADDR = "https://www.flickr.com/explore/interesting/7days/"
+FLICKR_DOMAIN = "https://www.flickr.com"
 FLICKR_PHOTO_CLASS = ".Photo"
 
